c1dd06ca9eaa76cd5a6d8df081003ad08e4b9223
Egidijus <egidijus.brazaitis@gmail.com>
2017-09-09 14:03:32 +0300

    Uploading project to github

---
+++ MVClogin/Controllers/HomeController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVClogin.Models;
+using System.Data;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace MVClogin.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        // returns admin view. 
+        public ActionResult Admin()
+        {
+            return View();
+        }
+        // gets data from the database and counts approximate gross of each user. 
+        public ActionResult GetData()
+        {
+            using (LoginDataBaseEntities1 db = new LoginDataBaseEntities1())
+            {
+
+                List<User> empList = db.Users.ToList<User>();
+                foreach (User usr in empList)
+                {
+                    usr.Gross = approxGross(usr.Salary);
+                }
+                return Json(new { data = empList }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Add(int id = 0)
+        {
+            return View(new User());
+        }
+        [HttpPost]
+        //adds a new user to database
+        public ActionResult Add(User emp)
+        {
+            using (LoginDataBaseEntities1 db = new LoginDataBaseEntities1())
+            {
+                emp.Password = Encrypt(emp.Password);
+
+                if (db.Users.Where(x => x.UserName == emp.UserName).FirstOrDefault() == null)
+                {
+                    db.Users.Add(emp);
+                    db.SaveChanges();
+                    return Json(new { success = true, message = "Saved Successfully" }, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    return Json(new { success = false, message = "Username already exists" }, JsonRequestBehavior.AllowGet);
+
+                }
+            }
+        }
+
+        [HttpGet]
+        // returns the information about editing user
+        public ActionResult Edit(int id)
+        {
+            using (LoginDataBaseEntities1 db = new LoginDataBaseEntities1())
+            {
+                return View(db.Users.Where(x => x.UserID == id).FirstOrDefault<User>());
+            }
+        }
+        [HttpPost]
+        //updates user's data
+        public ActionResult Edit(User emp)
+        {
+
+            using (LoginDataBaseEntities1 db = new LoginDataBaseEntities1())
+            {
+                db.Entry(emp).State = EntityState.Modified;
+                db.SaveChanges();
+                return Json(new { success = true, message = "Updated Successfully" }, JsonRequestBehavior.AllowGet);
+
+            }
+
+
+        }
+        [HttpPost]
+        // deletes user from database
+        public ActionResult Delete(int id)
+        {
+            using (LoginDataBaseEntities1 db = new LoginDataBaseEntities1())
+            {
+                User emp = db.Users.Where(x => x.UserID == id).FirstOrDefault<User>();
+                db.Users.Remove(emp);
+                db.SaveChanges();
+                return Json(new { success = true, message = "Deleted Successfully" }, JsonRequestBehavior.AllowGet);
+            }
+        }
+        // returns managers page.
+        public ActionResult Manager()
+        {
+            return View();
+        }
+        // returns workers page.
+        public ActionResult Worker()
+        {
+            return View();
+        }
+        // encrypts user's password with md5 hash.
+        private static string Encrypt(string value)
+        {
+            using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+            {
+                UTF8Encoding utf8 = new UTF8Encoding();
+                byte[] data = md5.ComputeHash(utf8.GetBytes(value));
+                return Convert.ToBase64String(data);
+            }
+
+        }
+        // function that calculates approximate gross with given net 
+        public static double approxGross (double net)
+        {
+            double gross = 0;
+            if (net <= 310)
+            {
+                gross = net / 0.91;
+            }
+            else if (net > 310 && net <= 380)
+            {
+                gross = (net - 46.5) / 0.76;
+            }
+            else if (net > 380 && net < 760)
+            {
+                gross = (net - 75) / 0.685;
+            }
+            else
+            {
+                gross = net / 0.76;
+            }
+            gross =  Math.Round(gross, 2);
+            return gross;
+        }
+    }
+}
---
+++ MVClogin/Controllers/LoginController.cs
@@ -0,0 +1,73 @@
+﻿using MVClogin.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace MVClogin.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        // returns the login view.
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        // checks if given username and password matches in the database. If matches 
+        // redirects to the session depending on the role(admin, manager, worker), 
+        // if it doesn't, redirects to login page.
+        public ActionResult Autherize(MVClogin.Models.User userModel)
+        {
+            userModel.Password = Encrypt(userModel.Password);
+            using (LoginDataBaseEntities1 db = new LoginDataBaseEntities1())
+            {
+                var userDetails = db.Users.Where(x => x.UserName == userModel.UserName && x.Password == userModel.Password).FirstOrDefault();
+                if (userDetails == null)
+                {
+                    userModel.LoginErrorMessage = "Wrong username or password";
+                    userModel.Password = "";
+                    return View("Index", userModel);
+                }
+
+                else
+                {
+                    Session["UserID"] = userDetails.UserID;
+                    Session["Name"] = userDetails.Name;
+                    Session["Surname"] = userDetails.Surname;
+                    Session["Gross"] = userDetails.Salary * 3;
+                    Session["Salary"] = userDetails.Salary * 2;
+                    Session["Role"] = userDetails.Role;
+                    if (userDetails.Role == 1)
+                        return RedirectToAction("Admin", "Home");
+                    else if (userDetails.Role == 2)
+                        return RedirectToAction("Manager", "Home");
+                    return RedirectToAction("Worker", "Home");
+                }
+            }
+            
+        }
+        // abandons the session and returns to the login page
+        public ActionResult LogOut()
+        {
+            Session.Abandon();
+            return RedirectToAction("Index", "Login");
+        }
+        // encrypts the password with md5 hash. this functions is used to compare given password with the 
+        // database user's password.
+        private static string Encrypt(string value)
+        {
+            using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+            {
+                UTF8Encoding utf8 = new UTF8Encoding();
+                byte[] data = md5.ComputeHash(utf8.GetBytes(value));
+                return Convert.ToBase64String(data);
+            }
+
+        }
+    }
+}
